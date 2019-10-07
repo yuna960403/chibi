aa60c9f2e22928b3d3ed89d5a41b38390a9ae3e2
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-07 16:28:51 +0900

    Class Q5

--- rational.py
+++ rational.py
@@ -6,7 +6,7 @@ class Q(object):
         if self.b == 1:             #分母を消す
             return str(self.a)
         return f'{self.a}/{self.b}'
-    def add(self,q):
+    def __add__(self,q):
         a = self.a
         b = self.b
         c = q.a
@@ -15,4 +15,4 @@ class Q(object):
 
 q1 = Q(1,2)
 q2 = Q(1,3)
-print(q1.add(q2))       #==> 5/6
+print(q1+q2)       #==> 5/6
