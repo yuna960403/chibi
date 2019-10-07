302a7d919e1139d7db9767805b864b6bbf24eb87
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-07 16:15:26 +0900

    Class Q3

--- rational.py
+++ rational.py
@@ -1,9 +1,11 @@
 class Q(object):
-    def __init__(self,a,b):
+    def __init__(self,a,b=1):       #b=1で分母を無効化できる
         self.a = a
         self.b = b
     def __repr__(self):
+        if self.b == 1:             #分母を消す
+            return str(self.a)
         return f'{self.a}/{self.b}'
 
-q = Q(1,2)
+q = Q(3)
 print(q)
