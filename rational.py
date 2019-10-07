55c25cc7228e82cfb537894076c0d4be8bc22f6d
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-07 16:47:27 +0900

    Class Q8

--- rational.py
+++ rational.py
@@ -1,7 +1,10 @@
+import math
+
 class Q(object):
-    def __init__(self,a,b=1):       #b=1で分母を無効化できる
-        self.a = a
-        self.b = b
+    def __init__(self,a,b=1): 
+        gcd = math.gcd(a,b)      #b=1で分母を無効化できる
+        self.a = a//gcd
+        self.b = b//gcd
     def __repr__(self):
         if self.b == 1:             #分母を消す
             return str(self.a)
