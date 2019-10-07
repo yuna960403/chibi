95dbaf35a53bdcc51b380abdd38d1385317377b7
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-07 16:39:17 +0900

    Class Q7

--- rational.py
+++ rational.py
@@ -6,13 +6,34 @@ class Q(object):
         if self.b == 1:             #分母を消す
             return str(self.a)
         return f'{self.a}/{self.b}'
-    def __sub__(self,q):
+    def __add__(self,q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b 
         return Q(a*d+b*c,b*d)
+    def __sub__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*d-b*c,b*d)
+    def __mul__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*c,b*d)
+    def __truediv__(self,q):
+        a = self.a
+        b = self.b
+        c = q.a
+        d = q.b 
+        return Q(a*d,b*c)
 
 q1 = Q(1,2)
 q2 = Q(1,3)
-print(q1-q2)       #==> 5/6
+print(q1+q2)       #==> 5/6
+print(q1-q2)       #==> 1/6
+print(q1*q2)       #==> 1/6
+print(q1/q2)       #==> 3/2
