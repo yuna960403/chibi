58b03686f297b5bf42a7be99c6e2ab46b703f4f4
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-14 16:10:25 +0900

    Val Add

--- exp.py
+++ exp.py
@@ -10,4 +10,23 @@ class Val(object):
 
 v = Val(1)
 assert v.eval() == 1
-print(v)
+print(v) 
+
+class Add(object):
+    __slots__ = ['left','right']
+    def __init__(self,a,b):
+        self.left = a       #aとbは式
+        self.right = b
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+e = Add(Val(1),Val(2))        #1+2
+assert e.eval() == 3
+
+#次のステップ 1+2+3 ==> 6
+
+e = Add(Val(1),Add(Val(2),Val(3)))
+assert e.eval() == 6
+
+print()
+print()
