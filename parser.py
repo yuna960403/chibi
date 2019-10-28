b4a202d8cc5ba3ccc3f2571d325d243adb4a62c4
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-28 16:45:21 +0900

    parseSub

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val, Add, Sub, Mul, Div #+-*/
+from exp import Val, Add, Sub, Mul, Div #+,-,*,/
 
 def parse(s: str):
     num = int(s)
@@ -22,13 +22,14 @@ def parse(s:str):
         pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Mul(parse(s1),parse(s2))
-    if s.find('*') > 0:
-        pos = s.find('*')
+        return Sub(parse(s1),parse(s2))
+    if s.find('-') > 0:
+        pos = s.rfind('-')  #-を計算するときは、rfindで逆から求める
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Mul(parse(s1),parse(s2))
+        return Sub(parse(s1),parse(s2))
     return Val(int(s))
 
-e = parse("1*2+3")
-print(e,e.eval())
+e = parse("1-2-3")
+print(e,e.eval())   #-4
+assert e.eval() == -4
