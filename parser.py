ac89db03fe39be87aae404110b0fdc62bc6dfa83
SUGIMOTO Yuna <21716054@jwu.ac.jp>
2019-10-28 16:20:36 +0900

    parseAdd2

--- parser.py
+++ parser.py
@@ -1,16 +1,31 @@
-from exp import Val,Add
+from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
     num = int(s)
     return Val(num)
 
+'''
 e = parse("123")
 print(e)
 
 s = "123+456"
-pos = s.find('+')
-print(pos,pos)  # + 記号を探す
+pos = s.find('+')   # + 記号を探す
+print(pos,pos)
 
 s1 = s[0:pos]
 s2 = s[pos+1:]
-print(s,s1,s2)
+print(s,s1,s2)  # + 記号で分割
+'''
+
+def parse(s:str):
+    pos = s.find('+')
+    if pos == -1:
+        num = int(s)
+        return Val(num)
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1),parse(s2))
+
+e = parse("123+456+789")
+print(e)
